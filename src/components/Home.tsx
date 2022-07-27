8f2baf21430a033d50ba6b7af4245753266c561e
David <hello@david-sima.dev>
2022-07-27 21:18:56 +0300

    feat: improved Home tab by adding some styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,10 @@
 import { Text } from "@mantine/core";
+import { useDocumentTitle } from "@mantine/hooks";
 
 export function Home() {
+  useDocumentTitle("RMG Utils for Stellaris - Home");
   return (
-    <Text>
+    <Text align="center" size="xl" weight={700}>
       Welcome to RMG's utils for Stellaris. Use the tabs above to navigate.
     </Text>
   );
