16af2e7a0f33c552424fe3a583009548740b2b67
David <hello@david-sima.dev>
2023-06-13 20:38:10 +0300

    feat: added System class

---
+++ src/utils/map/System.ts
@@ -0,0 +1,64 @@
+export class System {
+  static baseRegExp = new RegExp(
+    `system\\s*=\\s*{\\s*id\\s*=\\s*"(?<id>[0-9]+)"\\s*name\\s*=\\s*"(?<name>[A-Za-z0-9' _.-]*)"`
+  );
+  static xPosRegExp = new RegExp(`x\\s*=\\s*(?<x>[-]?[0-9]+)`);
+  static yPosRegExp = new RegExp(`y\\s*=\\s*(?<y>[-]?[0-9]+)`);
+  static initRegExp = new RegExp(`initializer\\s*=\\s*(?<init>[A-Za-z0-9_]*)`);
+
+  #id!: number;
+  name!: string;
+  x!: number;
+  y!: number;
+  #line: string;
+  init?: string;
+
+  constructor(line: string) {
+    this.#line = line;
+    const baseMatch = System.baseRegExp.exec(line)?.groups;
+    if (baseMatch) {
+      this.#id = parseInt(baseMatch.id);
+      this.name = baseMatch.name;
+    } else {
+      this.#throwConstructorError(line + ` baseMatch`);
+    }
+
+    const xPosMatch = System.xPosRegExp.exec(line)?.groups;
+    if (xPosMatch) {
+      this.x = parseInt(xPosMatch.x) * -1;
+    } else {
+      this.#throwConstructorError(line + ` xPosMatch`);
+    }
+
+    const yPosMatch = System.yPosRegExp.exec(line)?.groups;
+    if (yPosMatch) {
+      this.y = parseInt(yPosMatch.y) * -1;
+    } else {
+      this.#throwConstructorError(line + ` yPosMatch`);
+    }
+
+    const initMatch = System.initRegExp.exec(line)?.groups;
+    if (initMatch) {
+      this.init = initMatch.init;
+    }
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  toString() {
+    return this.#line
+      .replace(
+        System.baseRegExp,
+        `system = { id = "${this.#id}" name = "${this.name}"`
+      )
+      .replace(System.xPosRegExp, `x = ${this.x * -1}`)
+      .replace(System.yPosRegExp, `y = ${this.y * -1}`)
+      .replace(System.initRegExp, `initializer = ${this.init}`);
+  }
+
+  #throwConstructorError = (line: string) => {
+    throw new Error(`System constructor failed to parse line: ${line}`);
+  };
+}
---
+++ src/utils/map/tests/System.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it, test } from "vitest";
+
+import { System } from "../System";
+
+const line = `system = { id = "1" name = "Galactic Center" initializer = galactic_center_initializer position = { y = 1 x = -1 } }`;
+
+describe("System", () => {
+  test("test RegExps", () => {
+    const baseMatch = System.baseRegExp.exec(line)?.groups;
+    expect(baseMatch).toEqual({ id: "1", name: "Galactic Center" });
+    const xPosMatch = System.xPosRegExp.exec(line)?.groups;
+    expect(xPosMatch).toEqual({ x: "-1" });
+    const yPosMatch = System.yPosRegExp.exec(line)?.groups;
+    expect(yPosMatch).toEqual({ y: "1" });
+    const initMatch = System.initRegExp.exec(line)?.groups;
+    expect(initMatch).toEqual({ init: "galactic_center_initializer" });
+  });
+  it("saves all the info passed to the constructor", () => {
+    const s = new System(line);
+    expect(s.id).toEqual(1);
+    expect(s.name).toEqual("Galactic Center");
+    // x and y are flipped in the constructor
+    expect(s.x).toEqual(1);
+    expect(s.y).toBe(-1);
+    expect(s.init).toEqual("galactic_center_initializer");
+  });
+  it("throws an error if the line is not parsable", () => {
+    expect(
+      () =>
+        new System(
+          `system = { id = "1" name = "Galactic Center" initializer = galactic_center_initializer }`
+        )
+    ).toThrow();
+  });
+  it("toString() returns the correct string", () => {
+    const s = new System(line);
+    s.name = "New Name";
+    expect(s.toString()).toBe(line.replace("Galactic Center", "New Name"));
+  });
+});
