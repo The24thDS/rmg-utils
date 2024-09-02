7d213ce3ffb4726c1db40d3b54da8b4214d18dd1
The24thDS <hello@david-sima.dev>
2024-09-02 19:37:02 +0300

    feat: added selection for systems

--- src/components/GalaxyBuilderTab/System/SystemMarker.tsx
+++ src/components/GalaxyBuilderTab/System/SystemMarker.tsx
@@ -1,20 +1,41 @@
 import { CircleMarker, Tooltip } from "react-leaflet";
-import { PrimitiveAtom, useAtomValue } from "jotai";
+import { PrimitiveAtom, useAtom } from "jotai";
 
 import { System } from "../../../utils/map/System";
+import { isSystemItem, selectedItemAtom } from "../../../store/galaxy.store";
+import { useCallback } from "react";
 
 export const SystemMarker = ({
   systemAtom,
 }: {
   systemAtom: PrimitiveAtom<System>;
 }) => {
-  const system = useAtomValue(systemAtom);
+  const [system, setSystem] = useAtom(systemAtom);
+  const [selectedItem, setSelectedItem] = useAtom(selectedItemAtom);
+  const isSelected =
+    isSystemItem(selectedItem) && selectedItem.id === system.id;
+
+  const setAsSelected = useCallback(() => {
+    setSelectedItem({
+      type: "system",
+      id: system.id,
+      atom: systemAtom,
+    });
+  }, [system.id, systemAtom, setSelectedItem]);
 
   return system ? (
     <CircleMarker
       key={`${systemAtom}`}
       center={[system.y, system.x]}
       radius={3}
+      eventHandlers={{
+        click: setAsSelected,
+      }}
+      pathOptions={{
+        color: isSelected ? "orange" : "#3388ff",
+        fillColor: isSelected ? "orange" : "#3388ff",
+        fillOpacity: 0.5,
+      }}
     >
       <Tooltip pane="popups" className="rmg-leaflet-popup">
         {system.name}
--- src/store/galaxy.store.ts
+++ src/store/galaxy.store.ts
@@ -1,11 +1,12 @@
 import { PrimitiveAtom, atom } from "jotai";
 
 import { Nebula } from "../utils/map/Nebula";
+import { System } from "../utils/map/System";
 
-interface Item {
-  type: "nebula";
-  id: string;
-  atom: PrimitiveAtom<Nebula>;
+export interface Item {
+  type: "nebula" | "system";
+  id: string | number;
+  atom: PrimitiveAtom<Nebula> | PrimitiveAtom<System>;
 }
 
 type ActionType = "move" | "edit";
@@ -13,3 +14,7 @@ type ActionType = "move" | "edit";
 export const selectedItemAtom = atom<Item | null>(null);
 
 export const selectedActionAtom = atom<ActionType>("move");
+
+export function isSystemItem(item: Item | null): item is Item & { id: number; atom: PrimitiveAtom<System> } {
+  return !!item && typeof item.id === 'number' && item.type === 'system';
+}
